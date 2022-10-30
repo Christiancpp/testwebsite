01b698dbb1ee6ee4ececc0e44c4546fea592d357
Christiancpp <117039362+Christiancpp@users.noreply.github.com>
2022-10-30 15:21:03 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function christianfriends(){
+	document.getElementById("Christianf").onclick = website.location.href ="file:///C:/Users/Christian/Desktop/ChristianF.html";
+
+function dallyfriends(){
+	document.getElementById("Dallyf").innerHTML = "Clicked.";
+}
